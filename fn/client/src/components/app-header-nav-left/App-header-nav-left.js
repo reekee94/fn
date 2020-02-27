aaf2f2e439cc3b9b005b5f730c154bf3fae22d77
chak-kit <kitchakk93@gmail.com>
2020-02-28 00:28:12 +0200

    create drop-down menu refactoring

--- fn/client/src/components/app-header-nav-left/App-header-nav-left.js
+++ fn/client/src/components/app-header-nav-left/App-header-nav-left.js
@@ -2,70 +2,46 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './App-header-nav-left.css';
 import CategoriesNav from "../categories-nav";
-import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
 
 export default class AppHeaderNavLeft extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle1 = this.toggle1.bind(this);
-    this.onMouseEnter1 = this.onMouseEnter1.bind(this);
-    this.onMouseLeave1 = this.onMouseLeave1.bind(this);
-    this.toggle2 = this.toggle2.bind(this);
-    this.onMouseEnter2 = this.onMouseEnter2.bind(this);
-    this.onMouseLeave2 = this.onMouseLeave2.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
     this.state = {
-      dropdownOpen1: false,
-      dropdownOpen2: false
-
+      menDropdownOpen: false,
+      womenDropdownOpen: false,
+      kidsDropdownOpen: false
     };
   }
 
-  toggle1() {
-    this.setState(prevState => ({
-      dropdownOpen1: !prevState.dropdownOpen1
-    }));
+  toggle(key) {
+    this.updateState(key, !this.state[key]);
   }
 
-  onMouseEnter1() {
-    this.setState({
-      dropdownOpen1: true
-    });
+  onMouseEnter(key) {
+    this.updateState(key, true);
   }
 
-  onMouseLeave1(event) {
-    this.setState({
-      dropdownOpen1: false,
-      value1: event.target.innerText
-    });
+  onMouseLeave(key) {
+    this.updateState(key, false);
   }
 
-  toggle2() {
-    this.setState(prevState => ({
-      dropdownOpen2: !prevState.dropdownOpen1
-    }));
+  updateState(key, value) {
+    let newState = {};
+    newState[key] = value;
+    this.setState(newState);
   }
-
-  onMouseEnter2() {
-    this.setState({
-      dropdownOpen2: true
-    });
-  }
-
-  onMouseLeave2() {
-    this.setState({
-      dropdownOpen2: false
-    });
-
-  }
-
-
 
   render() {
     return (
       <div>
-        <Dropdown className="d-inline-block" onMouseOver={this.onMouseEnter1} onMouseLeave={this.onMouseLeave1}
-                  isOpen={this.state.dropdownOpen1} toggle={this.toggle1}>
+        <Dropdown className="d-inline-block" onMouseOver={() => this.onMouseEnter('menDropdownOpen')}
+                  onMouseLeave={() => this.onMouseLeave('menDropdownOpen')}
+                  isOpen={this.state.menDropdownOpen} toggle={() => this.toggle('menDropdownOpen')}>
 
           <DropdownToggle caret>
             <Link to="/catalogs/men">Men</Link>
@@ -75,8 +51,9 @@ export default class AppHeaderNavLeft extends React.Component {
           </DropdownMenu>
         </Dropdown>
 
-        <Dropdown className="d-inline-block" onMouseOver={this.onMouseEnter2} onMouseLeave={this.onMouseLeave2}
-                  isOpen={this.state.dropdownOpen2} toggle={this.toggle2}>
+        <Dropdown className="d-inline-block" onMouseOver={() => this.onMouseEnter('womenDropdownOpen')}
+                  onMouseLeave={() => this.onMouseLeave('womenDropdownOpen')}
+                  isOpen={this.state.womenDropdownOpen} toggle={() => this.toggle('womenDropdownOpen')}>
           <DropdownToggle caret>
             <Link to="/catalogs/women">Women</Link>
           </DropdownToggle>
