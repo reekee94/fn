dcd9718950d275088a85f4fe46962190f87b26b4
YarkoSumyk <yarko.sumyk@gmail.com>
2020-02-27 17:33:20 +0200

    'sortByPrice'

--- fn/client/src/components/product-list/Product-list.js
+++ fn/client/src/components/product-list/Product-list.js
@@ -5,43 +5,52 @@ import './Product-list.css';
 import ProductListPosts from '../product-list-posts';
 import ProductListPaginator from '../product-list-paginator';
 import ProductListButtonPages from '../product-list-button-pages';
-
+import ProductSort from '../product-sort';
 import { productsLoaded } from '../../actions';
 import withStoreService from '../hoc';
 
 const ProductList = ({ storeService, productsLoaded, products }) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(15);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(15);
 
-    useEffect(() => {
-        storeService.getAllProducts()
-            .then(res => productsLoaded(res))
-    }, []);
+  useEffect(() => {
+    storeService.getAllProducts().then((res) => productsLoaded(res));
+  }, []);
 
-    // Get current posts
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = products.products.slice(indexOfFirstPost, indexOfLastPost);
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = products.products.slice(
+    indexOfFirstPost,
+    indexOfLastPost
+  );
 
-    // Change view
-    const paginateMethod = value => setCurrentPage(value);
-    const changeItemsMethod = number => setPostsPerPage(number);
-    const changePagination = () => setCurrentPage(1);
+  // Change view
+  const paginateMethod = (value) => setCurrentPage(value);
+  const changeItemsMethod = (number) => setPostsPerPage(number);
+  const changePagination = () => setCurrentPage(1);
 
-    return (
-        <section className="left-side">
-            <ProductListButtonPages
-                changeItems={changeItemsMethod}
-                changeCurrentPage={changePagination}
-                className="buttonsGroup"
-            />
-            <ProductListPosts products={currentPosts} />
-            <ProductListPaginator postPerPage={postsPerPage} totalPosts={products.products.length} paginate={paginateMethod} />
-        </section>
-    );
+  return (
+    <section className="left-side">
+      <ProductListButtonPages
+        changeItems={changeItemsMethod}
+        changeCurrentPage={changePagination}
+        className="buttonsGroup"
+      />
+      <ProductSort arrayToSort={currentPosts} />
+      <ProductListPosts products={currentPosts} />
+      <ProductListPaginator
+        postPerPage={postsPerPage}
+        totalPosts={products.products.length}
+        paginate={paginateMethod}
+      />
+    </section>
+  );
 };
 
 const mapStateToProps = ({ products }) => ({ products });
 const mapDispatchToProps = { productsLoaded };
 
-export default withStoreService()(connect(mapStateToProps, mapDispatchToProps)(ProductList));
+export default withStoreService()(
+  connect(mapStateToProps, mapDispatchToProps)(ProductList)
+);
